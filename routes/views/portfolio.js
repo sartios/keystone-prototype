a85c10946dcf04cae6e2729504dca8ea000a51a1
sartios <sartios@hotmail.com>
2016-09-08 17:17:48 +0300

    blog: refactor portfolio page to dynamic loading portfolio items

--- routes/views/portfolio.js
+++ routes/views/portfolio.js
@@ -3,6 +3,21 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res){
 
   var view = new keystone.View(req, res);
+  var locals = res.locals;
+
+  locals.data = {
+    portfolioItems: [],
+  };
+
+  view.on('init', function(next){
+
+    var q = keystone.list('PortfolioItem').model.find().populate('tags', 'name');
+
+    q.exec(function(err, results){
+      locals.data.portfolioItems = results;
+      next(err);
+    });
+  });
 
   view.render('portfolio');
 };
